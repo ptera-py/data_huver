f1868f38b2046fe5a129898af8e8fd523e10957c
ptera-py <kurzy@yandex.ru>
2019-11-05 23:36:33 +0300

    Factoring

--- lab/get.py
+++ lab/get.py
@@ -19,10 +19,13 @@ def get_stores():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
     }
-    r_stores_list = r_stores.get('https://lenta.com/api/v1/stores', headers=headers)
-    string_stores = r_stores_list.text
-    list_stores = json.loads(string_stores)  # Список словарей с магазинами
-    return list_stores
+    try:
+        r_stores_list = r_stores.get('https://lenta.com/api/v1/stores', headers=headers)
+        string_stores = r_stores_list.text
+        list_stores = json.loads(string_stores)  # Список словарей с магазинами
+        return list_stores
+    except requests.exceptions.ConnectionError: #Сервер бывает рвёт соедниение, в этом случае возвращаем err, который нужно обработать
+        return 'err'
 
 
 #Функция получения цены товара
@@ -31,24 +34,27 @@ def get_price(store, productLink):
     r_price.headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0' #Чтобы лента не блокировала запросы
     }
-    r_setcook_store = r_price.put('https://lenta.com/api/v1/me/store', json={"storeId": store['id']}) #Устанавливаем магазин
-    r_prod_site = r_price.get(productLink)
-    soup = BeautifulSoup(r_prod_site.text)
-    # Пытаемся распарсить цену
     try:
-        price = soup.find('dd', {'class': 'price__primary'}).text
-        price = price.replace(' ', '')
-        price = ''.join([c for c in price if ord(c) > 31 or ord(c) == 9])
-    except AttributeError:  # если такого товара нет в магазине
-        price = 'na'
-    # Пытаемся распарсить наименование товара
-    try:
-        productName = soup.find('div', {'class': 'sku-card__title'}).text
-        productName = ''.join([c for c in productName if ord(c) > 31 or ord(c) == 9])
-        productName = productName.replace('  ', '')
-    except AttributeError:  # если такого товара нет в каталоге
-        productName = 'na'
-    return {'productName': productName.encode('utf-8'), 'price': price.encode('utf-8')}
+        r_setcook_store = r_price.put('https://lenta.com/api/v1/me/store', json={"storeId": store['id']}) #Устанавливаем магазин
+        r_prod_site = r_price.get(productLink)
+        soup = BeautifulSoup(r_prod_site.text)
+        # Пытаемся распарсить цену
+        try:
+            price = soup.find('dd', {'class': 'price__primary'}).text
+            price = price.replace(' ', '')
+            price = ''.join([c for c in price if ord(c) > 31 or ord(c) == 9])
+        except AttributeError:  # если такого товара нет в магазине
+            price = 'na'
+        # Пытаемся распарсить наименование товара
+        try:
+            productName = soup.find('div', {'class': 'sku-card__title'}).text
+            productName = ''.join([c for c in productName if ord(c) > 31 or ord(c) == 9])
+            productName = productName.replace('  ', '')
+        except AttributeError:  # если такого товара нет в каталоге
+            productName = 'na'
+        return {'productName': productName.encode('utf-8'), 'price': price.encode('utf-8')}
+    except requests.exceptions.ConnectionError: #Сервер бывает рвёт соедниение, в этом случае возвращаем err, который нужно обработать
+        return 'err'
 
 
 #Перечень товаров
@@ -61,22 +67,32 @@ list_productsLinks = [
 
 
 token = '---' #Авторизация для YaDisk
-path = 'data_sets' #Папка на YaDisk
+path = 'data_sets/lenta' #Папка на YaDisk
 list_stores = get_stores() #Получаем список магазинов
 
 #Запускаем цикл по магазинам
 for (count, store) in enumerate(list_stores):
-    now = str(datetime.datetime.now()) #Текущее время
-    nowС = now.replace(' ', '').replace('-', '').replace(':', '').replace('.', '')  # Текущее время, очищенное от знаков препинания
-    filename = nowС + '.txt'  # Формируем имя файла из текущего времени
     ###### Часть для тестирования
-    if count > 5:
+    if count > 2:
         break
     ######
-    result = get_price(store,list_productsLinks[0]) #Получаем цену и название продукта
-    inputStr = nowС.encode('utf-8') + '<>'.encode('utf-8') + result['price'] + '<>'.encode('utf-8') + result['productName'] + '<>'.encode('utf-8') + str(store['id']).encode('utf-8') #Строка для записи в Файл
-    create_yadisk(token, path, filename, inputStr) #Записываем цену и название продукта на YaDisk
-    time.sleep(2) #Засыпаем на 2сек, чтоб не дидосить сайт
+    #Запускаем цикл по продуктам
+    for product in list_productsLinks:
+        now = str(datetime.datetime.now())  # Текущее время
+        nowС = now.replace(' ', '').replace('-', '').replace(':', '').replace('.', '')  # Текущее время, очищенное от знаков препинания
+        filename = nowС + '.txt'  # Формируем имя файла из текущего времени
+        result = get_price(store,product) #Получаем цену и название продукта
+
+        #Если сервер разовал соединение, попробуем несколько раз (10) его повторно опросить
+        for scount in range(10):
+            if(result != 'err'):
+                break #Если get_price не вернул err, то сворачиваем цикл и едем дальше
+            time.sleep(3) #Если get_price вернул err, то ждём 3сек и вызываем get_price повторно
+            result = get_price(store, product)
+
+        inputStr = 'lenta'.encode('utf-8') + '<>'.encode('utf-8') + nowС.encode('utf-8') + '<>'.encode('utf-8') + result['price'] + '<>'.encode('utf-8') + result['productName'] + '<>'.encode('utf-8') + str(store['id']).encode('utf-8') #Строка для записи в Файл
+        create_yadisk(token, path, filename, inputStr) #Записываем цену и название продукта на YaDisk
+        time.sleep(3) #Засыпаем на 3сек, чтоб не дидосить сайт
 
 #Записываем перечень магазинов на YaDisk
-create_yadisk(token,path,'stores.txt',str(list_stores).encode('utf-8'))
+create_yadisk(token,path,'stores' + nowС + '.txt' ,str(list_stores).encode('utf-8'))
